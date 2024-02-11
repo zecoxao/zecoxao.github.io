67baafbcfe1701ec50bd06060c3a5b4605c82939
zecoxao <zecoxao1@gmail.com>
2024-02-11 23:25:51 +0000

    add etaHEN 1.6B

--- ps5jb/payload_map.js
+++ ps5jb/payload_map.js
@@ -3,10 +3,10 @@ const payload_map =
         {
             displayTitle: 'etaHEN',
             description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 3.xx / 4.xx Only.',
-            fileName: 'etaHEN-1.5B.bin',
+            fileName: 'etaHEN-1.56.bin',
             author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
             source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.5 beta'
+            version: '1.6 beta'
         },
         {
             displayTitle: 'K-Stuff',
