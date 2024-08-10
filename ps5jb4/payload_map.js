1654ca96ccb2158434894c9fdb65a30b72e74c46
Your Name <you@example.com>
2024-08-10 10:30:11 +0100

    retail

---
+++ ps5jb4/payload_map.js
@@ -0,0 +1,84 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'etaHEN',
+            description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 3.xx / 4.xx Only.',
+            fileName: 'etaHEN-1.7B.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.7 beta'
+        },
+        {
+            displayTitle: 'K-Stuff',
+            description: 'FW 3.00, 3.20, 3.21 & 4.03-4.51 Only (Browser crashes on 3.xx). FPKG enabler',
+            fileName: 'payload.bin',
+            author: 'sleirsgoevy',
+            source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
+            version: '1.2'
+        },
+        {
+            displayTitle: 'libhijacker game-patch',
+            description: 'Patches supported games to run at higher framerates, and adds debug menus to certain titles.',
+            fileName: 'libhijacker-1.138.elf',
+            author: 'illusion0001, astrelsky',
+            source: 'https://github.com/illusion0001/libhijacker/releases',
+            version: '1.138'
+        },
+        {
+            displayTitle: 'ps5debug',
+            description: 'Debugger (Experimental beta)',
+            fileName: 'ps5debug.elf',
+            author: 'SiSTR0, ctn123',
+            source: 'https://github.com/GoldHEN/ps5debug',
+            version: '1.0b1'
+        },
+        {
+            displayTitle: 'etaHEN Testkit',
+            description: 'Runs With 3.xx and 4.xx. FPKG enabler For FW 3.xx / 4.xx Only. Testkit Support Added.',
+            fileName: 'Testkit-etaHEN.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion, notnotzecoxao, Anonymous',
+            source:'https://github.com/LightningMods/etaHEN',
+            version: '1.7 beta'
+        },
+        {
+            displayTitle: 'FTPS5 (Non-Persistent)',
+            description: 'FTP Server',
+            fileName: 'ftps5-np.elf',
+            author: 'SiSTR0, zecoxao, EchoStretch',
+            source:'https://github.com/EchoStretch/FTPS5/releases',
+            version: '1.4'
+        },
+        {
+            displayTitle: 'FTPS5 (Persistent)',
+            description: 'FTP Server',
+            fileName: 'ftps5-p.elf',
+            author: 'SiSTR0, zecoxao, EchoStretch',
+            source:'https://github.com/EchoStretch/FTPS5/releases',
+            version: '1.4'
+        },
+        {
+            displayTitle: 'Versions',
+            description: 'Shows kernel build, os and sdk versions',
+            fileName: 'versions.elf',
+            author: 'SiSTRo',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
+        {
+            displayTitle: 'GetOSVersion',
+            description: 'very slow',
+            fileName: 'getOsVersion.elf',
+            author: 'logic-68',
+            source:'https://github.com/logic-68/getOsVersion/releases',
+            version: '1.0.2'
+        },
+        {
+            displayTitle: 'PS5_Browser_appCache_remove',
+            description: 'Removes appcache from browser.',
+            fileName: 'Browser_appCache_remove.elf',
+            author: 'Storm21CH',
+            source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
+            version: '1.0fix'
+        }
+
+    ];
