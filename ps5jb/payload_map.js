dfcd3a695ab488ea53fdaff6498e21bffc62197d
Your Name <you@example.com>
2025-02-14 17:02:25 +0000

    some updates

--- ps5jb/payload_map.js
+++ ps5jb/payload_map.js
@@ -3,10 +3,10 @@ const payload_map =
         {
             displayTitle: 'etaHEN',
             description: 'Runs With 3.xx and 4.xx on this host. FPKG enabler For FW 3.xx / 4.xx Only.',
-            fileName: 'etaHEN-1.9B.bin',
+            fileName: 'etaHEN.bin',
             author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
             source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.9 beta'
+            version: '2.0 beta'
         },
         {
             displayTitle: 'etaHEN Devkit',
