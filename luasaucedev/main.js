161b643e36964c87424918fa643c6bb5fedd4a3e
Your Name <you@example.com>
2025-04-17 19:07:35 +0100

    lua sauce 6

--- luasaucedev/main.js
+++ luasaucedev/main.js
@@ -573,15 +573,16 @@ async function main(userlandRW, wkOnly = false) {
         await krw.write4(get_kaddr(OFFSET_KERNEL_SECURITY_FLAGS), security_flags | 0x14);
 
         // Set targetid to DEX
-        //await krw.write1(get_kaddr(OFFSET_KERNEL_TARGETID), 0x81);
+        await krw.write1(get_kaddr(OFFSET_KERNEL_TARGETID), 0x82);
 
         // Set qa flags and utoken flags for debug menu enable
-        //let qaf_dword = await krw.read4(get_kaddr(OFFSET_KERNEL_QA_FLAGS));
-        //await krw.write4(get_kaddr(OFFSET_KERNEL_QA_FLAGS), qaf_dword | 0x10300);
+        await krw.write8(get_kaddr(OFFSET_KERNEL_QA_FLAGS    ), new int64(0xFFFFFFFF, 0xFFFFFFFF));
+        await krw.write8(get_kaddr(OFFSET_KERNEL_QA_FLAGS + 8), new int64(0xFFFFFFFF, 0xFFFFFFFF));
 
         //let utoken_flags = await krw.read1(get_kaddr(OFFSET_KERNEL_UTOKEN_FLAGS));
-        //await krw.write1(get_kaddr(OFFSET_KERNEL_UTOKEN_FLAGS), utoken_flags | 0x1);
-        //await log("Enabled debug menu", LogLevel.INFO);
+        await krw.write8(get_kaddr(OFFSET_KERNEL_UTOKEN_FLAGS    ), new int64(0xFFFFFFFF, 0xFFFFFFFF));
+		await krw.write8(get_kaddr(OFFSET_KERNEL_UTOKEN_FLAGS + 8), new int64(0xFFFFFFFF, 0xFFFFFFFF));
+        await log("Enabled debug menu", LogLevel.INFO);
 
         // Patch creds
         let cur_uid = await chain.syscall(SYS_GETUID);
